a9dad08d94991d34a89058af94779d978c3d18ca
wlgns522 <wlgns5221122@gmail.com>
2024-03-22 16:52:56 +0900

    hi

---
+++ proper.py
@@ -0,0 +1,10 @@
+a = ['1','2','3',] # 집합 객체
+b = ['1','2','3','4','5'] # 집합 객체
+union = list(set(a)|set(b)) # 합집합
+intersection = list(set(a)&set(b)) # 교집합
+complement = list(set(a)-set(b))# 차집합
+proper = a>b
+print("합집합:",union)
+print("교집합:",intersection)
+print("차집합:",complement)
+print("진부분집합:", proper)
