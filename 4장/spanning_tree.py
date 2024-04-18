ddc27a1704d7ee7a7d106f075761d7efc5703386
wlgns522 <wlgns5221122@gmail.com>
2024-04-18 17:25:14 +0900

    44

--- 4장/spanning_tree.py
+++ 4장/spanning_tree.py
@@ -1,30 +1,31 @@
-def dfs(graph, vertex, visited, spanning_tree):
+def dfs(graph, start, visited, spanning_tree):
+    visited[start] = True 
 
-    visited[vertex] = True
-    
-    for adjacent, weight in enumerate(graph[vertex]):
-        if weight != 0 and not visited[adjacent]:
-            spanning_tree[vertex][adjacent] = weight
-            spanning_tree[adjacent][vertex] = weight
-            dfs(graph, adjacent, visited, spanning_tree)
+    for neighbor in range(len(graph[start])):
+        if graph[start][neighbor] != 0 and not visited[neighbor]:
+            spanning_tree[start][neighbor] = 1  
+            spanning_tree[neighbor][start] = 1  
+            dfs(graph, neighbor, visited, spanning_tree)
 
-def find_spanning_tree(graph):
+def prim(graph):
     num_vertices = len(graph)
-    visited = [False] * num_vertices
-    spanning_tree = [[0] * num_vertices for _ in range(num_vertices)]
+    visited = [False] * num_vertices 
+    spanning_tree = [[0] * num_vertices for _ in range(num_vertices)] 
 
     start_vertex = 0
     dfs(graph, start_vertex, visited, spanning_tree)
 
     return spanning_tree
+
 graph = [
-    [0, 2, 0, 1, 0],
-    [2, 0, 3, 4, 0],
-    [0, 3, 0, 0, 5],
-    [1, 4, 0, 0, 6],
-    [0, 0, 5, 6, 0]
+    [0, 2, 0, 6, 0],
+    [2, 0, 3, 8, 5],
+    [0, 3, 0, 0, 7],
+    [6, 8, 0, 0, 9],
+    [0, 5, 7, 9, 0]
 ]
-spanning_tree = find_spanning_tree(graph)
+spanning_tree = prim(graph)
+
 print("신장 트리:")
 for row in spanning_tree:
     print(row)
